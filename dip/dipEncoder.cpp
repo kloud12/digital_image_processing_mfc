3243b4a29fa4ada8b9c93f5e70ac0c945a8768a2
yeongkloud <youngkloud@dauml.net>
2017-01-25 15:43:22 +0900

    Basic

---
+++ dip/dipEncoder.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "dipEncoder.h"
+
+
+dipEncoder::dipEncoder()
+{
+}
+
+
+dipEncoder::~dipEncoder()
+{
+}
---
+++ dip/dipEncoder.h
@@ -0,0 +1,8 @@
+#pragma once
+class dipEncoder
+{
+public:
+	dipEncoder();
+	~dipEncoder();
+};
+
